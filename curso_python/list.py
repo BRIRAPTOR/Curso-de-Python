3c0a3d2aada606df9694c9e380e9434e5f60b1d1
Brian Hazel <briraptor21@gmail.com>
2024-04-28 16:38:59 -0600

    constructor list

---
+++ curso_python/list.py
@@ -0,0 +1,12 @@
+print(range(0, 100, 10))
+
+numeros = range(0, 100, 10)
+print(numeros)
+
+print(list(range(0, 100, 10)))
+
+nombre = input('Ingrese el nombre: ')
+print(list(nombre))
+list_nombre = list(nombre)
+print(list_nombre[:: -1])
+print(list())
